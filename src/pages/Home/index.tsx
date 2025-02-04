0bcc35032122e5282f9810e2478fdaa04ad3b8e1
juniorcintra <junior.cintra.developer@gmail.com>
2025-02-04 12:21:10 -0300

    aprimorando inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,12 +19,24 @@ export default function Home() {
             id="task"
             type="text"
             placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
           />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+          </datalist>
+
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             id="minutesAmount"
             type="number"
             placeholder="00"
+            min={5}
+            max={60}
+            step={5}
           />
 
           <span>minutos.</span>
