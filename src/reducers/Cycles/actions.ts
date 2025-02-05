b2c7ed7229da7953a946164bc4966b0b4f719a69
juniorcintra <junior.cintra.developer@gmail.com>
2025-02-05 18:28:36 -0300

    correcao de tipagem

--- src/reducers/Cycles/actions.ts
+++ src/reducers/Cycles/actions.ts
@@ -1,10 +1,12 @@
+import { Cycle } from "../../@types";
+
 export enum ActionTypes {
   ADD_NEW_CYCLE = "ADD_NEW_CYCLE",
   INTERRUPT_CURRENT_CYCLE = "INTERRUPT_CURRENT_CYCLE",
   MARK_CURRENT_CYCLE_AS_FINISHED = "MARK_CURRENT_CYCLE_AS_FINISHED",
 }
 
-export function addNewCycleAction(newCycle: any) {
+export function addNewCycleAction(newCycle: Cycle) {
   return {
     type: ActionTypes.ADD_NEW_CYCLE,
     payload: newCycle,
